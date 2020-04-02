f2a6cc57b97e16d79e6431830a2b203fe0dd60d3
TommasoAzz <azzalintommaso@gmail.com>
2020-04-02 14:01:45 +0200

    Aggiornato stalker.yaml

--- model/src/main/java/it/qbteam/model/OrganizationAccess.java
+++ model/src/main/java/it/qbteam/model/OrganizationAccess.java
@@ -95,7 +95,6 @@ public class OrganizationAccess   {
    * @return exitTimestamp
   */
   @ApiModelProperty(required = true, value = "Date and time of the moment in which the user left the place.")
-  @NotNull
 
   @Valid
 
