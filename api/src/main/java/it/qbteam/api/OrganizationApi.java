7bf5f7ac083a2d6b6911b43274c64e0ef21f7fc8
TommasoAzz <azzalintommaso@gmail.com>
2020-04-03 22:34:48 +0200

    #112 interfacce API

--- api/src/main/java/it/qbteam/api/OrganizationApi.java
+++ api/src/main/java/it/qbteam/api/OrganizationApi.java
@@ -11,25 +11,17 @@ import it.qbteam.model.Organization;
 import it.qbteam.model.Permission;
 import it.qbteam.model.Place;
 import io.swagger.annotations.*;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.context.request.NativeWebRequest;
-import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 import javax.validation.constraints.*;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 
 @Validated
 @Api(value = "organization", description = "the organization API")
