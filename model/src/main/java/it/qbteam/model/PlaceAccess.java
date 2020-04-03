e21d5fb5edb01bcd9929d3647de3dafe4862a0a9
TommasoAzz <azzalintommaso@gmail.com>
2020-04-03 22:44:03 +0200

    #1 aggiornate entità

--- model/src/main/java/it/qbteam/model/PlaceAccess.java
+++ model/src/main/java/it/qbteam/model/PlaceAccess.java
@@ -20,7 +20,7 @@ import javax.validation.constraints.*;
 @Entity
 public class PlaceAccess   {
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @GeneratedValue(strategy = GenerationType.AUTO)
   @JsonProperty("id")
   private Long id;
 
