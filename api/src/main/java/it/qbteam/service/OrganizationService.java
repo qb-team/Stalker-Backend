8d62caa4475db53c021449202367509ddaabea90
dlazzaro <dmlazzaro98@gmail.com>
2020-04-17 18:14:31 +0200

    implementati Organization service e favourite service

--- api/src/main/java/it/qbteam/service/OrganizationService.java
+++ api/src/main/java/it/qbteam/service/OrganizationService.java
@@ -1,6 +1,8 @@
 package it.qbteam.service;
 
 import it.qbteam.model.Organization;
+import it.qbteam.repository.sql.OrganizationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -8,9 +10,14 @@ import java.util.Optional;
 
 @Service
 public interface OrganizationService {
-    Optional<Organization> getOrganization(Long organizationId);
-    Optional<List<Organization>> getOrganizationList();
+
+    Optional <Organization> getOrganization(Long organizationId);
+
+    List<Organization> getOrganizationList();
+
     void requestDeletionOfOrganization(Long organizationId, String requestReason);
+
     Optional<Organization> updateOrganization(Long organizationId,  Organization organization);
+
     Optional<Organization> updateOrganizationTrackingArea(Long organizationId, String trackingArea);
 }
--- api/src/main/java/it/qbteam/serviceImpl/OrganizationServiceImpl.java
+++ api/src/main/java/it/qbteam/serviceImpl/OrganizationServiceImpl.java
@@ -3,8 +3,10 @@ package it.qbteam.serviceImpl;
 import it.qbteam.model.Organization;
 import it.qbteam.repository.sql.OrganizationRepository;
 import it.qbteam.service.OrganizationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,14 +16,25 @@ public class OrganizationServiceImpl implements OrganizationService {
 
     private OrganizationRepository organizationRepo;
 
-    @Override
-    public Optional<Organization> getOrganization(Long organizationId) {
-        return Optional.empty();
+    @Autowired
+    public OrganizationServiceImpl( OrganizationRepository organizationRepository){
+        this.organizationRepo= organizationRepository;
     }
 
     @Override
-    public Optional<List<Organization>> getOrganizationList() {
-        return Optional.empty();
+    public Optional <Organization> getOrganization(Long organizationId) {
+
+        return organizationRepo.findById(organizationId);
+    }
+
+    @Override
+    public List<Organization> getOrganizationList() {
+
+        Iterable<Organization> lista = organizationRepo.findAll();
+        List<Organization> returnList = new ArrayList<>();
+        lista.forEach(returnList::add);
+        return returnList;
+
     }
 
     @Override
@@ -31,11 +44,31 @@ public class OrganizationServiceImpl implements OrganizationService {
 
     @Override
     public Optional<Organization> updateOrganization(Long organizationId, Organization organization) {
-        return Optional.empty();
+        Optional<Organization> original= organizationRepo.findById(organizationId);
+        Organization prova = null;
+        if(original.isPresent()){prova = original.get();}
+        prova.setAuthenticationServerURL(organization.getAuthenticationServerURL());
+        prova.setCity(organization.getCity());
+        prova.setCountry(organization.getCountry());
+        prova.setCreationDate(organization.getCreationDate());
+        prova.setDescription(organization.getDescription());
+        prova.setImage(organization.getImage());
+        prova.setName(organization.getName());
+        prova.setNumber(organization.getNumber());
+        prova.setPostCode(organization.getPostCode());
+        prova.setStreet(organization.getStreet());
+        prova.setTrackingArea(organization.getTrackingArea());
+        prova.setTrackingMode(organization.getTrackingMode());
+        return Optional.of(prova);
     }
 
     @Override
     public Optional<Organization> updateOrganizationTrackingArea(Long organizationId, String trackingArea) {
-        return Optional.empty();
+        Optional<Organization> original= organizationRepo.findById(organizationId);
+        Organization prova = null;
+        if(original.isPresent()){prova = original.get();}
+        prova.setTrackingArea(trackingArea);
+        return Optional.of(prova);
+
     }
 }
