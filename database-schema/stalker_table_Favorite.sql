9c19e7115b4179451ececd5fb6d5d241d6fbe70f
TommasoAzz <azzalintommaso@gmail.com>
2020-04-30 17:40:24 +0200

    Corretto errore in schema Favorite

--- database-schema/stalker_table_Favorite.sql
+++ database-schema/stalker_table_Favorite.sql
@@ -2,6 +2,6 @@
 CREATE TABLE `Favorite` (
   `userId` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Authentication service''s user unique identifier.',
   `organizationId` bigint NOT NULL COMMENT 'Unique identifier of the organization the user sets as favorite.',
-  `orgAuthId` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'User unique identifier from the authentication server of the organization.',
+  `orgAuthServerId` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'User unique identifier from the authentication server of the organization.',
   `creationDate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'When the favorite was added by the user.'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Link between the user and the organization: only the organization which users set to be their favorite can track their movements.';
