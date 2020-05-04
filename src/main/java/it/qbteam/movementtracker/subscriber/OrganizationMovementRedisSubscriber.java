82ef5709acfe3ca539c2af6eb0ab2b1a55010266
dlazzaro <dmlazzaro98@gmail.com>
2020-05-04 22:55:24 +0200

    costruttori in redis subscriber

--- src/main/java/it/qbteam/movementtracker/subscriber/OrganizationMovementRedisSubscriber.java
+++ src/main/java/it/qbteam/movementtracker/subscriber/OrganizationMovementRedisSubscriber.java
@@ -1,11 +1,24 @@
 package it.qbteam.movementtracker.subscriber;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.stereotype.Service;
+
+import it.qbteam.repository.OrganizationAccessRepository;
+import it.qbteam.repository.PlaceAccessRepository;
 
 @Service
 public class OrganizationMovementRedisSubscriber extends OrganizationMovementSubscriber {
 
+    private OrganizationAccessRepository organizationAccessRepository;
+    private PlaceAccessRepository placeAccessRepository;
+
+    @Autowired
+    public OrganizationMovementRedisSubscriber(OrganizationAccessRepository org, PlaceAccessRepository place){
+        this.organizationAccessRepository= org;
+        this.placeAccessRepository = place;
+    }
+    
     @Override
     public void onMessage(Message message, byte[] pattern) {
         System.out.println("REDIS MESSAGE: " + message.toString());
--- src/main/java/it/qbteam/movementtracker/subscriber/PlaceMovementRedisSubscriber.java
+++ src/main/java/it/qbteam/movementtracker/subscriber/PlaceMovementRedisSubscriber.java
@@ -1,11 +1,25 @@
 package it.qbteam.movementtracker.subscriber;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.connection.Message;
 
 import org.springframework.stereotype.Service;
 
+import it.qbteam.repository.OrganizationAccessRepository;
+import it.qbteam.repository.PlaceAccessRepository;
+
 @Service
 public class PlaceMovementRedisSubscriber extends PlaceMovementSubscriber {
+
+    private OrganizationAccessRepository organizationAccessRepository;
+    private PlaceAccessRepository placeAccessRepository;
+
+    @Autowired
+    public PlaceMovementRedisSubscriber(OrganizationAccessRepository org, PlaceAccessRepository place){
+        this.organizationAccessRepository= org;
+        this.placeAccessRepository = place;
+    }
+
     @Override
     public void onMessage(Message message, byte[] pattern) {
         System.out.println("REDIS MESSAGE: " + message.toString());
