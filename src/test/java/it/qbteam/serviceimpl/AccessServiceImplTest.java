7c913d1b3a4de924a9dbb47dfcd65e8c651d6953
dlazzaro <dmlazzaro98@gmail.com>
2020-05-26 17:26:00 +0200

    #117 finiti test AccessServiceImpl

--- src/test/java/it/qbteam/serviceimpl/AccessServiceImplTest.java
+++ src/test/java/it/qbteam/serviceimpl/AccessServiceImplTest.java
@@ -17,6 +17,10 @@ import it.qbteam.model.OrganizationAccess;
 import it.qbteam.model.PlaceAccess;
 import it.qbteam.repository.OrganizationAccessRepository;
 import it.qbteam.repository.PlaceAccessRepository;
+import sun.awt.image.ImageWatched;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
 
 @RunWith(SpringRunner.class)
 public class AccessServiceImplTest {
@@ -50,6 +54,12 @@ public class AccessServiceImplTest {
 
         Assert.assertEquals(outputList, accessService.getAuthenticatedAccessListInOrganization(ids, 1L));
     }
+    @Test
+    public void testGetAuthenticatedAccessListInOrganizationReturnsEmptyAccessListGivenEmptyOrgAuthServerIds() {
+        List<OrganizationAccess> outputList = new LinkedList<>();
+        List<String> ids = new LinkedList<>();
+        Assert.assertEquals(outputList, accessService.getAuthenticatedAccessListInOrganization(ids, 1L));
+    }
     
     @Test
     public void testGetAuthenticatedAccessListInPlaceReturnsAccessList() {
@@ -63,5 +73,44 @@ public class AccessServiceImplTest {
         Mockito.when(placeAccessRepository.findByOrgAuthServerIdAndPlaceId("orgAuthServerId", 1L)).thenReturn(outputList);
 
         Assert.assertEquals(outputList, accessService.getAuthenticatedAccessListInPlace(ids, 1L));
+    }
+    @Test
+    public void testGetAuthenticatedAccessListInPlaceReturnsEmptyAccessListGivenOrgAuthServerIds() {
+        List<PlaceAccess> outputList = new LinkedList<>();
+        List<String> ids = new LinkedList<>();
+        Assert.assertEquals(outputList, accessService.getAuthenticatedAccessListInPlace(ids, 1L));
+    }
+
+    @Test
+    public void testGetAnonymousAccessListInOrganization(){
+        List<String> exitTokens = new LinkedList<>();
+        List<OrganizationAccess> organizationAccessList = new LinkedList<>();
+        Iterable<OrganizationAccess> organizationAccessIterable= new LinkedList<>();
+        Assert.assertEquals(organizationAccessList, accessService.getAnonymousAccessListInOrganization(exitTokens, 1l));
+
+        OrganizationAccess testOrganizationAccess = new OrganizationAccess().organizationId(1l).exitToken("prova").orgAuthServerId("prova");
+        organizationAccessList.add(testOrganizationAccess);
+        organizationAccessIterable=organizationAccessList;
+        exitTokens.add("prova");
+
+        Mockito.when(organizationAccessRepository.findByExitTokenAndOrganizationId(anyString(), anyLong())).thenReturn(organizationAccessIterable);
+        Assert.assertEquals(organizationAccessList, accessService.getAnonymousAccessListInOrganization(exitTokens, 1l));
+    }
+
+    @Test
+    public void testGetAnonymousAccessListInPlace(){
+        List<String> exitTokens = new LinkedList<>();
+        List<PlaceAccess> placeAccessList = new LinkedList<>();
+        Iterable<PlaceAccess> placeAccessIterable= new LinkedList<>();
+        Assert.assertEquals(placeAccessList, accessService.getAnonymousAccessListInPlace(exitTokens, 1l));
+
+        PlaceAccess testPlaceAccess = new PlaceAccess().id(1l).placeId(1l).exitToken("prova").orgAuthServerId("prova");
+        placeAccessList.add(testPlaceAccess);
+        placeAccessIterable=placeAccessList;
+        exitTokens.add("prova");
+
+        Mockito.when(placeAccessRepository.findByExitTokenAndPlaceId(anyString(), anyLong())).thenReturn(placeAccessIterable);
+        Assert.assertEquals(placeAccessList, accessService.getAnonymousAccessListInPlace(exitTokens, 1l));
+
     }
 }
